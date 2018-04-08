b56970def004c77067999cba8aa16668d7ee186c
Sheikh-Umar <sheikh.umar.banafe@gmail.com>
2018-04-08 19:58:57 +0800

    Test cases for phone number and email address combinations

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -31,4 +31,49 @@ public class FindCommandParserTest {
         assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
     }
 
+    //@@author Sheikh-Umar
+    @Test
+    public void parse_validArgs_returnsFindCommandForPhoneNumber() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("99991234", "88776655")));
+        assertParseSuccess(parser, "99991234 88776655", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n 99991234 \n \t 88776655  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommandForEmailAddress() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("johntan@gmail.com", "ericlee@hotmail.com")));
+        assertParseSuccess(parser, "johntan@gmail.com ericlee@hotmail.com", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n johntan@gmail.com \n \t ericlee@hotmail.com  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommandForEmailAddressAndPhoneNumber() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("johntan@gmail.com", "99001234")));
+        assertParseSuccess(parser, "johntan@gmail.com 99001234", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n johntan@gmail.com \n \t 99001234  \t", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommandForPhoneNumberAndEmailAddress() {
+        // no leading and trailing whitespaces
+        FindCommand expectedFindCommand =
+                new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("87650099", "ericlee@gmail.com")));
+        assertParseSuccess(parser, "87650099 ericlee@gmail.com", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n 87650099 \n \t ericlee@gmail.com  \t", expectedFindCommand);
+    }
+    //@@author
 }
