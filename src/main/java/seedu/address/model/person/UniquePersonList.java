3e66d5afc2e37a94c47a86e89ef117df3a0df3a1
Sheikh-Umar <sheikh.umar.banafe@gmail.com>
2018-04-10 07:24:07 +0800

    Edited variable name

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -33,9 +33,9 @@ public class UniquePersonList implements Iterable<Person> {
     public boolean contains(Person toCheck) {
         requireNonNull(toCheck);
         for (int i = 0; i < internalList.size(); i++) {
-            Person current = internalList.get(i);
-            if (current.getPhone().equals(toCheck.getPhone())
-                    || current.getEmail().equals(toCheck.getEmail())) {
+            Person currentPersonInCRM = internalList.get(i);
+            if (currentPersonInCRM.getPhone().equals(toCheck.getPhone())
+                    || currentPersonInCRM.getEmail().equals(toCheck.getEmail())) {
                 return true;
             }
         }
